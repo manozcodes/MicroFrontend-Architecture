8993816d31bf36b08d58990248d2436b3f75bcbf
Manoz Acharya <manozacharya1998@gmail.com>
2021-04-02 00:05:54 +0545

    setting up microfrontend architecture in reactjs using lerna, craco, and typescript

---
+++ packages/module1/src/App.tsx
@@ -0,0 +1,23 @@
+import { Test } from "@manozcodes/shared";
+
+function index() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>Module -1</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Test />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default index;
---
+++ packages/module1/src/components/Test1.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test1: React.FC = (props) => {
+    return (
+        <div>
+            <b>This is Module 1.</b>
+        </div>
+    )
+}
+
+export default Test1;
